855ff72f10e921a9e305fd820021b8b3fc5cf875
jwetter21 <108046115+jwetter21@users.noreply.github.com>
2025-10-18 16:50:42 -0700

    added input block_size to __init__ and specified input value in write as a bytearray object

--- lstore/page.py
+++ lstore/page.py
@@ -1,14 +1,15 @@
 
 class Page:
 
-    def __init__(self):
+    def __init__(self, block_size):
         self.num_records = 0
         self.data = bytearray(4096)
+        self.block_size = block_size
 
     def has_capacity(self):
         pass
 
-    def write(self, value):
+    def write(self, value: bytearray):
         self.num_records += 1
         pass
 
