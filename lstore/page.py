94af0297220f5b8a7a2ae708fb9cdb5bc69a5826
jwetter21 <108046115+jwetter21@users.noreply.github.com>
2025-10-18 19:37:48 -0700

    wrote initial implementation for write()

--- lstore/page.py
+++ lstore/page.py
@@ -1,7 +1,7 @@
 
 class Page:
 
-    def __init__(self, block_size):
+    def __init__(self, block_size: int):
         self.num_records = 0
         self.data = bytearray(4096)
         self.block_size = block_size
@@ -10,6 +10,16 @@ class Page:
         pass
 
     def write(self, value: bytearray):
+        """
+        Takes a bytearray and writes into the next available block of bytes.
+        """
+        # FIXME: Needs to raise an exception if len(value) != block_size
+        # start_byte will hold the first byte of the incoming value bytearray
+        start_byte = self.block_size * self.num_records
+        # Insert one byte at a time
+        for b in range(len(value)):
+            self.data[start_byte + b] = value[b]
+        
         self.num_records += 1
-        pass
 
+        return True
