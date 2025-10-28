2a37819ba82ed5d637ae6ac3d3b051a5434c902e
jwetter21 <108046115+jwetter21@users.noreply.github.com>
2025-10-28 14:22:19 -0700

    wrote and implemented select_version which is only slightly different than first part of code for Table.update() written by Jared

--- lstore/table.py
+++ lstore/table.py
@@ -31,4 +31,21 @@ class Table:
     def __merge(self):
         print("merge is happening")
         pass
- 
+
+    def select_version(self, primary_key, relative_version=0):
+        base_record = self.bp_directory[self.key_rid[primary_key][0]]
+        
+        if abs(relative_version) >= len(self.tp_directory[base_record.rid]):
+            # if relative_version is out of range, return the base_record
+            return base_record
+        else:
+            requested_tail = None
+
+            if base_record.rid in self.tp_directory:
+                # relative_version = -1 will return the second to most recent version
+                requested_tail = self.tp_directory[base_record.rid][relative_version-1]
+            else:
+                requested_tail = base_record.copy()
+
+            return requested_tail
+
