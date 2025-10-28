3f8f8cd56002df9430896205ed16b1e3b15f0ff2
jwetter21 <108046115+jwetter21@users.noreply.github.com>
2025-10-27 20:10:51 -0700

    cleaned up comments

--- lstore/query.py
+++ lstore/query.py
@@ -58,22 +58,25 @@ class Query:
     # Assume that select will never be called on a key that doesn't exist
     """
     def select(self, search_key, search_key_index, projected_columns_index):
+        # get the indices of all matching records
         indices = self.table.index.locate(search_key_index, search_key)
+
+        # get records from indices
         records = []
         for i in indices:
             records.append(self.table.get_record(i))
 
         records_dict = {}
 
-        # Sort out the returned indices
+        # Sort out the returned indices uding records_dict
         for record in records:
             if record.rid not in records_dict.keys():
                 records_dict[record.rid] = [record]
             else:
                 records_dict[record.rid].append(record)
         
+        # Take the most recent record from each collection of records
         return_list = []
-
         for k in records_dict.keys():
             records_dict[k].sort()
             return_list.append(records_dict[k][-1])
