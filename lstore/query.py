40268633d7644423cd596870c246bc3f68cc2532
jwetter21 <108046115+jwetter21@users.noreply.github.com>
2025-10-28 14:55:35 -0700

    added initial implementation for select and select_version, might need changes depending on if and how Table.select_version() is implemented

--- lstore/query.py
+++ lstore/query.py
@@ -52,7 +52,12 @@ class Query:
     # Assume that select will never be called on a key that doesn't exist
     """
     def select(self, search_key, search_key_index, projected_columns_index):
-        pass
+        try:
+            self.table.select_version(search_key, search_key_index, projected_columns_index)
+        except Exception:
+            return False
+        else:
+            return True
 
     
     """
@@ -66,7 +71,12 @@ class Query:
     # Assume that select will never be called on a key that doesn't exist
     """
     def select_version(self, search_key, search_key_index, projected_columns_index, relative_version):
-        pass
+        try:
+            self.table.select_version(search_key, search_key_index, projected_columns_index, relative_version)
+        except Exception:
+            return False
+        else:
+            return True
 
     
     """
