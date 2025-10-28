950c68679b357b8abcf2faaa755fc81245e5c733
jwetter21 <108046115+jwetter21@users.noreply.github.com>
2025-10-28 14:30:32 -0700

    added implementation for sum

--- lstore/query.py
+++ lstore/query.py
@@ -94,7 +94,27 @@ class Query:
     # Returns False if no record exists in the given range
     """
     def sum(self, start_range, end_range, aggregate_column_index):
-        pass
+        # Set up
+        column_to_get = [0]*self.table.num_columns
+        column_to_get[aggregate_column_index] = 1
+        sum = 0
+        record_exists = False
+
+        for t in range(start_range, end_range + 1):
+            # only accesses the needed column
+            try:
+                # select only the needed column using primary key
+                # self.select(t, self.table.key, column_to_get) should return a list containing one record object
+                sum += self.select(t, self.table.key, column_to_get)[0].columns[0]
+                record_exists = True
+            except:
+                continue
+
+        if record_exists:
+            return sum
+        else:
+            # If there are no entries within the range return False
+            return False
 
     
     """
