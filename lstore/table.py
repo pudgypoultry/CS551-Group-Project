95223f7f028f2aab1ec493e54e2d8b483827d24f
jwetter21 <108046115+jwetter21@users.noreply.github.com>
2025-10-28 14:57:00 -0700

    made select_version into function stub with correct parameters

--- lstore/table.py
+++ lstore/table.py
@@ -32,20 +32,22 @@ class Table:
         print("merge is happening")
         pass
 
-    def select_version(self, primary_key, relative_version=0):
-        base_record = self.bp_directory[self.key_rid[primary_key][0]]
+    def select_version(self, key, key_index, projected_columns, relative_version=0):
+        pass
+        # # FIXME: The following code only works for primary_key
+        # base_record = self.bp_directory[self.key_rid[primary_key][0]]
         
-        if abs(relative_version) >= len(self.tp_directory[base_record.rid]):
-            # if relative_version is out of range, return the base_record
-            return base_record
-        else:
-            requested_tail = None
+        # if abs(relative_version) >= len(self.tp_directory[base_record.rid]):
+        #     # if relative_version is out of range, return the base_record
+        #     return base_record
+        # else:
+        #     requested_tail = None
 
-            if base_record.rid in self.tp_directory:
-                # relative_version = -1 will return the second to most recent version
-                requested_tail = self.tp_directory[base_record.rid][relative_version-1]
-            else:
-                requested_tail = base_record.copy()
+        #     if base_record.rid in self.tp_directory:
+        #         # relative_version = -1 will return the second to most recent version
+        #         requested_tail = self.tp_directory[base_record.rid][relative_version-1]
+        #     else:
+        #         requested_tail = base_record.copy()
 
-            return requested_tail
+        #     return requested_tail
 
