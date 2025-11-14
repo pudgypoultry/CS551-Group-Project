04936838f29a631a8762e17d7d5040fbaaecdfbb
jwetter21 <108046115+jwetter21@users.noreply.github.com>
2025-11-14 13:10:31 -0800

    cleaned up commented out code from select, select_version; changed sum and sum_version so they take advantage of columnar store

--- lstore/query.py
+++ lstore/query.py
@@ -72,28 +72,6 @@ class Query:
 
                 records.append(record)
         return records
-        # # try:
-        #     records = []
-        #     RIDs = self.table.index.locate(search_key_index, search_key)
-        #     for rid in RIDs:
-        #         # This if/else checks if the record has ever been updated
-        #         if len(self.table.recordDirectory[rid]) == 0:
-        #             record = self.table.fetch(rid, 0) 
-        #         else:
-        #             record = self.table.fetch(rid) 
-
-        #         # FIXME: This process of only selecting the desired columns WILL be optimized so as to capitalize on columnar approach
-        #         new_columns = []
-        #         for i in range(self.table.numColumns):
-        #             if projected_columns_index[i] == 1:
-        #                 new_columns.append(record.columns[i])
-
-        #         # has_key = projected_columns_index[self.table.primaryKey] == 1
-        #         # new_record_key = record.key if has_key else None
-        #         # FIXME: The primary key is set to be the original primary key, even though the shortened record may not contain it.
-        #         new_record = Record(rid, record.key, new_columns)
-        #         records.append(new_record)
-        #     return records
 
     
     """
@@ -140,31 +118,6 @@ class Query:
                 records.append(record)
 
         return records
-        # records = []
-        # RIDs = self.table.index.locate(search_key_index, search_key)
-        # for rid in RIDs:
-        #     # This if/else checks if relative_version is out of range => return base record
-        #     if len(self.table.recordDirectory[rid]) <= abs(relative_version):
-        #         # Case 1: return the base record
-        #         rVersion = 0
-        #     else:
-        #         # Case 2: return a tail record
-        #         rVersion = relative_version - 1
-
-        #     record = self.table.fetch(rid, rVersion)
-
-        #     # FIXME: This process of only selecting the desired columns WILL be optimized so as to capitalize on columnar approach
-        #     new_columns = []
-        #     for i in range(self.table.numColumns):
-        #         if projected_columns_index[i] == 1:
-        #             new_columns.append(record.columns[i])
-
-        #     # has_key = projected_columns_index[self.table.primaryKey] == 1
-        #     # new_record_key = record.key if has_key else None
-        #     # FIXME: The primary key is set to be the original primary key, even though the shortened record may not contain it.
-        #     new_record = Record(rid, record.key, new_columns)
-        #     records.append(new_record)
-        # return records
 
     """
     # Update a record with specified key and columns
@@ -198,8 +151,9 @@ class Query:
             # only accesses the needed column
             try:
                 # select only the needed column using primary key
-                # self.select(t, self.table.key, column_to_get) should return a list containing one record object
-                val_to_add = self.select(t, self.table.primaryKey, [1]*self.table.numColumns)[0].columns[aggregate_column_index]
+                # self.select(t, self.table.primaryKey, column_to_get) returns a list containing one record object, which has one column
+                val_to_add = self.select(t, self.table.primaryKey, column_to_get)[0].columns[0]
+                # val_to_add = self.select(t, self.table.primaryKey, [1]*self.table.numColumns)[0].columns[aggregate_column_index]
                 sum += val_to_add
                 record_exists = True
             except:
@@ -210,6 +164,29 @@ class Query:
         else:
             # If there are no entries within the range return False
             return False
+        
+        #         # Set up
+#         column_to_get = [0]*self.table.numColumns
+#         column_to_get[aggregate_column_index] = 1
+#         sum = 0
+#         record_exists = False
+
+#         for t in range(start_range, end_range + 1):
+#             # only accesses the needed column
+#             try:
+#                 # select only the needed column using primary key
+#                 # self.select(t, self.table.key, column_to_get) should return a list containing one record object
+#                 val_to_add = self.select(t, self.table.primaryKey, column_to_get)[0].columns[0]
+#                 sum += val_to_add
+#                 record_exists = True
+#             except:
+#                 continue
+
+#         if record_exists:
+#             return sum
+#         else:
+#             # If there are no entries within the range return False
+#             return False
 
     
     """
@@ -232,8 +209,9 @@ class Query:
             # only accesses the needed column
             try:
                 # select only the needed column using primary key
-                # self.select(t, self.table.key, column_to_get) should return a list containing one record object
-                val_to_add = self.select_version(t, self.table.primaryKey, [1]*self.table.numColumns, relative_version)[0].columns[aggregate_column_index]
+                # self.select(t, self.table.primaryKey, column_to_get, relative_version) should return a list containing one record object
+                val_to_add = self.select_version(t, self.table.primaryKey, column_to_get, relative_version)[0].columns[0]
+                # val_to_add = self.select_version(t, self.table.primaryKey, [1]*self.table.numColumns, relative_version)[0].columns[aggregate_column_index]
                 sum += val_to_add
                 record_exists = True
             except:
