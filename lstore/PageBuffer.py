8a6f9becebed14bcff62a14ebd33437b399c8244
pudgypoultry <50252390+pudgypoultry@users.noreply.github.com>
2025-11-30 15:11:31 -0800

    current

--- lstore/PageBuffer.py
+++ lstore/PageBuffer.py
@@ -1,4 +1,5 @@
 from lstore.page import Page
+from threading import Lock
 
 class PageBuffer():
     """
@@ -10,68 +11,71 @@ class PageBuffer():
         self.pages_path = pages_path  # Path to pages directory
         self.buffer = []  # List of Page objects
         self.page_ids = []  # Corresponding page IDs
+        self.lock = Lock() # Lock for thread safety
 
     def request_page(self, page_id):
         """Get a page from buffer or load from disk."""
+        with self.lock:
+            # Already in buffer? (Cache hit)
+            if page_id in self.page_ids:
+                idx = self.page_ids.index(page_id)
+                page = self.buffer.pop(idx)
+                self.page_ids.pop(idx)
+                # Move to end (most recently used)
+                self.buffer.append(page)
+                self.page_ids.append(page_id)
+                return page
 
-        # Already in buffer? (Cache hit)
-        if page_id in self.page_ids:
-            idx = self.page_ids.index(page_id)
-            page = self.buffer.pop(idx)
-            self.page_ids.pop(idx)
-            # Move to end (most recently used)
-            self.buffer.append(page)
-            self.page_ids.append(page_id)
+            # Cache miss - create page object and load from disk
+            page = Page(page_id, path=self.pages_path)
+            page.load()
+
+            # Add to buffer (evict if full)
+            self._add_to_buffer(page, page_id)
+
             return page
-
-        # Cache miss - create page object and load from disk
-        page = Page(page_id, path=self.pages_path)
-        page.load()
-
-        # Add to buffer (evict if full)
-        self._add_to_buffer(page, page_id)
-
-        return page
 
     def new_page(self, page_id):
         """Create a new page and add to buffer."""
+        with self.lock:
+            # Create new page
+            page = Page(page_id, path=self.pages_path)
 
-        # Create new page
-        page = Page(page_id, path=self.pages_path)
+            # Add to buffer (evict if full)
+            self._add_to_buffer(page, page_id)
 
-        # Add to buffer (evict if full)
-        self._add_to_buffer(page, page_id)
-
-        return page
+            return page
 
     def delete_page(self, page_id):
         """Delete a page from buffer and disk."""
-
-        # Remove from buffer if present
-        if page_id in self.page_ids:
-            idx = self.page_ids.index(page_id)
-            page = self.buffer.pop(idx)
-            self.page_ids.pop(idx)
-            page.delete()
-        else:
-            # Not in buffer, create temp page object to delete
-            page = Page(page_id, path=self.pages_path)
-            page.delete()
+        with self.lock:
+            # Remove from buffer if present
+            if page_id in self.page_ids:
+                idx = self.page_ids.index(page_id)
+                page = self.buffer.pop(idx)
+                self.page_ids.pop(idx)
+                page.delete()
+            else:
+                # Not in buffer, create temp page object to delete
+                page = Page(page_id, path=self.pages_path)
+                page.delete()
 
         return True
 
     def flush_all(self):
         """Write all dirty pages to disk"""
-        flushed = 0
-        for page in self.buffer:
-            if page.isdirty:
-                page.save()
-                page.isdirty = False
-                flushed += 1
-        return flushed
+        with self.lock:
+            flushed = 0
+            for page in self.buffer:
+                if page.isdirty:
+                    page.save()
+                    page.isdirty = False
+                    flushed += 1
+            return flushed
 
     def _add_to_buffer(self, page, page_id):
         """Add page to buffer, evicting LRU page if full"""
+        # Note: This is an internal method, called inside the lock of caller
         # Evict if full
         if len(self.buffer) >= self.capacity:
             evicted = self.buffer.pop(0)
